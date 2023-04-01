6260e9266d39562352d6b801feac08d95296fa92
Pakorn Lertkhachonsuk <128368465+pakornlkchs@users.noreply.github.com>
2023-04-02 00:07:27 +0700

    Update movies.sql

--- movies.sql
+++ movies.sql
@@ -52,3 +52,52 @@ FROM character
 WHERE movieid = 99;
 
 -- all data are cleaned, next step is exploratory data analysis
+
+-- EDA
+-- What is the average run time for all movies?
+SELECT 
+	ROUND(AVG(runtime),2) AS average_runtime
+FROM movies;
+
+-- What is the average runtime for each genre?
+SELECT 
+	genre,
+    	ROUND(AVG(runtime),2) AS average_runtime
+FROM movies
+GROUP BY genre
+ORDER BY average_runtime;
+
+-- TOP 10 movies with the most profit
+SELECT
+	title,
+    	(gross - budget) AS profit
+FROM movies
+ORDER BY profit DESC
+LIMIT 10;
+
+-- TOP 10 actors who played in the most movie
+SELECT 
+	a.name,
+    	COUNT(m.title) as number_of_movie_played,
+    	ROUND(AVG(rating),2) AS mean_rating
+FROM actor AS a
+Left JOIN character AS c 
+	ON a.actorid = c.actorid
+LEFT JOIN movies as m
+	ON c.movieid = m.movieid
+GROUP BY a.name
+ORDER BY number_of_movie_played DESC
+LIMIT 10;
+
+-- Who has played in movies the most? and what are that movies?
+SELECT 
+	a.name,
+	m.title,
+    	m.release_date,
+    	COUNT(m.title) OVER(PARTITION BY a.name) AS movies_played
+FROM actor AS a
+LEFT JOIN character AS c 
+	ON a.actorid = c.actorid
+LEFT JOIN movies as m
+	ON c.movieid = m.movieid
+ORDER BY movies_played DESC;
